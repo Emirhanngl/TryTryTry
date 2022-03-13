cca2cad9f12cd83101cbc3831a9f25d8de67c294
Emirhanngl <emir52ordu@hotmail.com>
2022-03-13 17:55:28 +0300

    Add project files.

---
+++ WebUI/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
